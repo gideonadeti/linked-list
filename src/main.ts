52c2b2c7d60f8ac03b0bccb248872f338f54f6e1
gideonadeti <gideonadeti0@gmail.com>
2024-10-15 12:29:04 +0000

    Work on toString function

--- src/main.ts
+++ src/main.ts
@@ -175,4 +175,18 @@ class LinkedList {
       current.nextNode = current.nextNode.nextNode;
     }
   }
+
+  toString() {
+    let result = [];
+    let current = this.head;
+
+    while (current !== null) {
+      result.push(`( ${current.value} )`);
+      current = current.nextNode;
+    }
+
+    result.push("null");
+
+    return result.join(" -> ");
+  }
 }
